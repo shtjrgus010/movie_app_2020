bbae26d6bc1ce2bc059deabc2f7c8b3c8feb131f
#1.0 Creating my first React App <https://github.com/shtjrgus010/movie_app_2020>
2020-08-18 15:06:02 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Potato from "./Potato";
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Potato />
     </div>
   );
 }
